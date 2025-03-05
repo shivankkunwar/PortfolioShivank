4fd11dd3bd95d32214fe226c4315b7603da1038f
Shivank Kunwar <shivankkunwar100@gmail.com>
2025-03-05 21:13:07 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-
-import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import ProjectsPage from "./components/Projects/ProjectsPage";
