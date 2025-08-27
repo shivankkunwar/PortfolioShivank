330a7d8e40c646a3d859c07d3544946b150d5a0f
shivank kunwar <shivankkunwar100@gmail.com>
2025-08-27 20:46:04 +0530

    asd

--- src/components/About/Intro.jsx
+++ src/components/About/Intro.jsx
@@ -3,7 +3,7 @@ function Intro() {
     <div className="intro-container">
       <div className="introTitle">
         <h1>Hi, I'm Shivank 👋</h1>
-        <h3>I build and innovate. (Frontend enthusiast exploring backend)</h3>
+        <h3>I build and innovate. (Creative frontend developer exploring backend)</h3>
       </div>
       <img className="introImg" src="/profile.jpg" alt="profile" />
     </div>
